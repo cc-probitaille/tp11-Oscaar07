77e5795606eab1e45e33c98b03cf645d901221e7
Oscar Do <oscardo322@gmail.com>
2024-11-21 14:35:54 -0500

    setup

---
+++ src/__tests__/creationClasseParReflection.test.ts
@@ -0,0 +1,14 @@
+import {creationClasseParReflection} from '../creationClasseParReflection';
+
+describe("String calculator", () => {
+    let c: creationClasseParReflection;
+    
+    beforeEach(() => {
+      c = new creationClasseParReflection();
+    });
+  
+    it("", () => {
+    });
+  
+  
+  });
